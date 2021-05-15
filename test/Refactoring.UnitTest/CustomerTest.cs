1356eb49b9a921c6caf11854a9b53f0aefa8d372
Amit P Naik <amit.naik8103@gmail.com>
2021-05-15 13:19:55 +0530

    feat: sample application

---
+++ test/Refactoring.UnitTest/CustomerTest.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace Refactoring.UnitTest
+{
+    public class CustomerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void checkStatement()
+        {
+            Customer customer = new Customer("Shyam");
+            Movie avengers = new Movie("Avengers", Movie.CHILDREN);
+            Rental rental = new Rental(avengers, 2);
+            customer.AddRental(rental);
+            Assert.That(customer.Statement, Is.EqualTo("Rental record for Shyam\n\tAvengers\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points"));
+
+        }
+
+        [Test]
+        public void getName()
+        {
+            Customer customer = new Customer("Shyam");
+            Assert.AreEqual("Shyam", customer.GetName());
+        }
+    }
+}
